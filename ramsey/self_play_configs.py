4c0ad4952ccd1601abfb247ef530eec8756a5180
Emily Hawboldt <emily.smith.3@louisville.edu>
2023-06-01 13:54:54 -0400

    Stable

--- ramsey/self_play_configs.py
+++ ramsey/self_play_configs.py
@@ -13,24 +13,34 @@ seemed to really clutter up this code, which is why I chose to just write
 the instructions there as opposed to including them here.    
     
 """
-trained_model = '2023_02_01_002_512' 
+model_save_dir = 'C:/users/emily/diss_repo/ramsey/models'
+model_name = 'initmodel_K5_3_3' 
 
-#headstart can bea string that leads to the gamestate, or it can be None
-headstart = None
+experience_save_dir = 'C:/users/emily/diss_repo/ramsey/experience'
 
-graph_order = 17
-clique_orders = (4, 4)
-encoder_name = 'k4_encoder'
+graph_order = 5
+clique_orders = (3, 3)
+encoder_name = 'k3_encoder'
 
-MCTS_rounds = 250
-MCTS_temp1 = 0.2
-MCTS_temp2 = 0.8
+MCTS_rounds = 100
+MCTS_temp1 = 0.5
+MCTS_temp2 = 0.5
 
-MCTS_temps = [MCTS_temp1, MCTS_temp2]
-num_workers = 4
-num_games = 2500
 first_move_random = False
+num_games = 8
+num_workers = 4
+games_per_batch = 1
+
+################################################################################
+################################################################################
+##################### leave this stuff alone :) ################################
+################################################################################
+################################################################################
 #%% Leave this part alone:
+MCTS_temps = [MCTS_temp1, MCTS_temp2]
+#headstart can be a string that leads to the gamestate, or it can be None
+headstart = None
+trained_model = f'{model_save_dir}/{model_name}'
 gpus = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(gpus[0], True) # multiprocessing GPU stuff
 sys.path.append(os.getcwd()) # helps multiprocessing find all the modules
@@ -42,18 +52,18 @@ else:
         head_start = dill.load(f)
     f.close()
 
-def main(num_games, num_workers, exp_dir):
-    os.mkdir(f'C:/users/emily/ramsey/ramsey_2p_rigid_rules/experience/{exp_dir}')
+def main(num_games, num_workers, games_per_batch, exp_dir):
+    full_exp_dir = f'{experience_save_dir}/{exp_dir}'
+    os.mkdir(f'{full_exp_dir}')
     workers = []
     games_per_worker = num_games//num_workers
-    games_per_batch = 10
     num_batches = games_per_worker//games_per_batch
     for i in range(num_workers):
         worker_ID = i+1
         worker = mp.Process(target = parallel_self_play_games,
                             args = (trained_model, encoder_name, graph_order,
                                     clique_orders, MCTS_rounds, MCTS_temps,
-                                num_batches, games_per_batch, exp_dir, worker_ID,
+                                num_batches, games_per_batch, full_exp_dir, worker_ID,
                                 head_start, first_move_random))
         worker.start()
         workers.append(worker)
@@ -64,8 +74,8 @@ if __name__ == '__main__':
     start = time.time()
     date_time = time.strftime("%d%b%Y_%H%M")
     exp_dir = f'{date_time}_{num_games}games'
-    main(num_games, num_workers, exp_dir)
-    combine_files(exp_dir)
+    main(num_games, num_workers, games_per_batch, exp_dir)
+    combine_files(f'{experience_save_dir}/{exp_dir}')
     end = time.time()
     seconds = end - start
     minutes = seconds/60
--- ramsey/z_init.py
+++ ramsey/z_init.py
@@ -4,10 +4,11 @@ from botparts.board import Board
 from botparts.encoders.base import Encoder
 from models.four_blocks import fresh_model
 
+model_save_dir = 'C:/users/emily/diss_repo/ramsey/models'
 
-graph_order = 17
-cliques_order = (4, 4)
-encoder_name = 'k4_encoder'
+graph_order = 5
+cliques_order = (3, 3)
+encoder_name = 'k3_encoder'
 
 value_hidden_layer = 128
 policy_hidden_layer = 128
@@ -22,6 +23,6 @@ model = fresh_model(encoder, value_hidden_layer, policy_hidden_layer)
 if isinstance(cliques_order, int):
     cliques_order = (cliques_order, cliques_order)
 
-directory = f'C:/users/emily/ramsey/ramsey_2p_rigid_rules/models/TEST_K{graph_order}_{cliques_order[0]}_{cliques_order[1]}'
+directory = f'{model_save_dir}/initmodel_K{graph_order}_{cliques_order[0]}_{cliques_order[1]}'
 os.mkdir(directory)
 model.save(directory)
