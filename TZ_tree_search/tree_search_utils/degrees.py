4a34d88712c82cee52d397ebed785abab66d9102
Emily Hawboldt <emily.smith.3@louisville.edu>
2023-06-14 11:02:40 -0400

    Cleanup

--- TZ_tree_search/tree_search_utils/degrees.py
+++ TZ_tree_search/tree_search_utils/degrees.py
@@ -3,12 +3,9 @@ def get_max_degree_node(graph, nodes):
     max_degree = 0
     max_node = None
     for n, degree in degrees:
-        # print(f'      Node {n} has degree {degree}')
         if degree > max_degree:
             max_degree = degree
-            # print(f'          Node {n} has the new max degree of {degree}')
             max_node = n
-    # print(f'         Returning max node {max_node}')
     return max_degree
 
 def get_min_degree_node(graph, nodes):
@@ -16,10 +13,7 @@ def get_min_degree_node(graph, nodes):
     min_degree = graph.order()
     min_node = None
     for n, degree in degrees:
-        # print(f'      Node {n} has degree {degree}')
         if degree < min_degree:
             min_degree = degree
-            # print(f'          Node {n} has the new max degree of {degree}')
             min_node = n
-    # print(f'         Returning max node {max_node}')
     return min_degree
